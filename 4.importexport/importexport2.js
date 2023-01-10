b839e19ff7782e8545cde427f0331245605dd88d
Shashank Tripathi <60851994+st908503@users.noreply.github.com>
2023-01-10 15:28:31 +0530

    Add files via upload

---
+++ 4.importexport/importexport2.js
@@ -0,0 +1,15 @@
+//  const name = require('./importexport1');
+// console.log(name.sub(9,3))
+// console.log(name.add(7,9));  
+// console.log(me);
+
+
+//or
+const {add,sub,me} = require ('./importexport1');
+
+console.log(add(6,8));
+console.log(sub(6,9))
+console.log(me)
+
+
+
